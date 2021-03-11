3cd77c7a8ff3bc4211ab54be6a9e0f1ae569b248
SeishinF <fernatoolsgamer@gmail.com>
2021-03-11 11:05:59 -0600

    Agregue etiquetas de Head
    Agregue los metas y los iconos.

--- views/public/index.php
+++ views/public/index.php
@@ -1,10 +1,25 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 <head>
+    <!-- Codificacion de caracteres -->
     <meta charset="UTF-8">
+    <!-- Espacio para agregar los LINK -->
+    <!-- Iconos de Google -->
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <!-- Vista de Compatibilidad -->
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <!-- Optimizacion en equipos pequeñes -->
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <!-- Descripcion de nuestro sitio web -->
+    <meta name="description" content="Green Zone, es una tienda en linea dedicada a la venta de productos ecoamigables, con el objetivo de promover el uso de estos y combatir pasivamente la contaminacion de ciertos produtos">
+    <!-- Palabras claves de nuestro sitio web -->
+    <meta name="keywords" content="Ecologico, Ambiente, Reciclaje, Tienda, Productos, Tienda en linea, Productos ecoamigables">
+    <!-- Autor del sitio web -->
+    <meta name="author" content="Green Zone Team">
+    <!-- Copyright -->
+    <meta name="copyright" content="Green Zone Team 2021">
+    <!-- Titulo de nuestro Sitio Web -->
+    <title>Green Zone Store</title>
 </head>
 <body>
     
