db883a6e00ba74f200f047bb99f64b6ba6b0b16a
SeishinF <fernatoolsgamer@gmail.com>
2021-03-16 15:33:19 -0600

    Cambios EN HEADER Y FOOTER

---
+++ app/helpers/footer.php
@@ -0,0 +1,61 @@
+</main>
+    <footer class="text-center text-lg-start" id="ptFooter">
+        
+        <div class="container p-4  text-center">
+          
+          <div class="row">
+            <!-- Div para Informacion -->
+            <div class="col-12 col-lg-6 col-md-12">
+                <h1>¿Quienes Somos?</h1>
+                <p>Green Zone, es una tienda en linea dedicada a la venta de productos ecoamigables, con el objetivo de promover el uso de estos y combatir pasivamente la contaminacion de ciertos produtos</p>
+            </div>
+            <!-- Div para Contacto -->
+            <div class="col-12 col-lg-3 col-md-6 ">
+                <div class="row">
+                    <div class="col-12">
+                        <h5 class="text-uppercase">Contactanos</h5>
+                    </div>
+                    <div class="col-4">
+                        <i class="fas fa-phone fa-4x"></i>
+                    </div>
+                    <div class="col-4">
+                        <i class="fab fa-whatsapp fa-4x"></i>
+                    </div>
+                    <div class="col-4">
+                        <i class="fas fa-envelope fa-4x"></i>
+                    </div>
+                </div>
+            </div>
+            <!-- Div para Redes Sociales -->
+            <div class="col-12 col-lg-3 col-md-6 mb-4 mb-md-0 ">
+                <div class="row">
+                    <div class="col-12">
+                        <h5 class="text-uppercase">Nuestras Redes Sociales</h5>
+                    </div>
+                    <div class="col-6">
+                        <i class="fab fa-instagram fa-4x"></i>
+                    </div>
+                    <div class="col-6">
+                        <i class="fab fa-facebook-square fa-4x"></i>
+                    </div>
+                </div>
+            </div>
+
+          </div>
+          
+        </div>
+        
+      
+        <!-- Derechos Reservados -->
+        <div class="text-center p-3" id="ptDerechos">
+        @2021 Derechos Reservados:
+          <a class="text-white" href="#">Green Zone </a>
+        </div>
+   </footer>
+
+    <!-- Agregamos SCRIPTS -->
+    <script src="https://kit.fontawesome.com/592eb2e9e3.js" crossorigin="anonymous"></script>
+    <script src="../../resources/js/bootstrap.min.js"></script>
+    <script src="../../resources/js/MenuInferior/mnInferior.js"></script>
+</body>
+</html>
---
+++ views/public/index.php
@@ -0,0 +1,13 @@
+<?php
+//Se incluye la plantilla del encabezado para la página web
+include("../../app/helpers/header.php");
+?>
+
+<div class="text-center" >
+        <img src="https://i.pinimg.com/originals/6f/7a/c4/6f7ac49c67bc4ed30eb9d80b0da3aba4.jpg" alt="" width="400px" height="700px" >
+        </div>
+
+<?php
+//Se incluye la plantilla del encabezado para la página web
+include("../../app/helpers/footer.php");
+?>
