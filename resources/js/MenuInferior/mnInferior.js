e46889af1d4f67ceeb29ef94cb0f431358de91db
SeishinF <fernatoolsgamer@gmail.com>
2021-03-16 21:34:15 -0600

    Cambios Finales

--- resources/js/MenuInferior/mnInferior.js
+++ resources/js/MenuInferior/mnInferior.js
@@ -1,10 +1,74 @@
 const btnCategorias = document.getElementById('btn-categorias'), 
-grid = document.getElementById('grid');
+grid = document.getElementById('grid'),
+contenedorEnlaces =  document.querySelector('#menu .contenedor-enlaces-nav'),
+btnCerrarMenu = document.getElementById('btn-menu-cerrar'),
+esDispositivoMovil = () => window.innerWidth<=800;
 
-btnCategorias.addEventListener('mouseenter', () =>{
+//Cuando se pasa el mouse se abra el menu
+btnCategorias.addEventListener('mouseover', () =>{
+    if(!esDispositivoMovil()){
+        grid.classList.add('activo');
+    }
+});
+
+//Cuando se salga del grid que se cierre el menu
+grid.addEventListener('mouseleave', () =>{
+    if(!esDispositivoMovil()){
+        grid.classList.remove('activo');
+    }
+});
+
+//Mostrar las categorias
+document.querySelectorAll('#menu .categorias a').forEach( (elemento) => {
+
+    elemento.addEventListener('mouseenter', (e) => {
+        
+        if(!esDispositivoMovil()){
+            document.querySelectorAll('#menu .subcategoria').forEach((categoria) =>{
+            
+                categoria.classList.remove('activo');
+                //mostrar subcategora por categoria
+                if(categoria.dataset.categoria == e.target.dataset.categoria){
+                    categoria.classList.add('activo');
+                }
+            });
+        }
+        
+    });
+
+});
+
+//Listerner de dispositivo movil
+
+document.querySelector('#btn-menu-barras').addEventListener('click', (e) =>{
+
+    e.preventDefault();
+    if(contenedorEnlaces.classList.contains('activo')){
+        contenedorEnlaces.classList.remove('activo');
+        document.querySelector('body').style.overflow = 'visible';
+    } else{
+        contenedorEnlaces.classList.add('activo');
+        document.querySelector('body').style.overflow = 'hidden';
+    }
+
+});
+
+//Click para todos los categorias
+
+btnCategorias.addEventListener('click' , (e) => {
+    e.preventDefault();
+    
     grid.classList.add('activo');
+    
+
 });
 
-btnCategorias.addEventListener('mouseleave', () =>{
+
+document.querySelector('#grid .categorias .btn-regresar').addEventListener('click' , (e) => {
+    e.preventDefault();
     grid.classList.remove('activo');
-});
+    
+})
+
+
+
--- views/public/AgregarProducto.php
+++ views/public/AgregarProducto.php
@@ -6,7 +6,7 @@ include("../../app/helpers/header.php");
 
  <div class="col-12 col-lg-12 col-md-12">
             <h1>¿Desea agregar un nuevo producto?</h1>
-        </div>
+ </div>
 
 <?php
 //Se incluye la plantilla del encabezado para la página web
