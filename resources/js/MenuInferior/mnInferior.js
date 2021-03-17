25ef7795127a7dfa198e09e9b863ff38984a1cfa
SeishinF <fernatoolsgamer@gmail.com>
2021-03-16 19:10:13 -0600

    Cambios PHP

--- resources/js/MenuInferior/mnInferior.js
+++ resources/js/MenuInferior/mnInferior.js
@@ -4,3 +4,7 @@ grid = document.getElementById('grid');
 btnCategorias.addEventListener('mouseenter', () =>{
     grid.classList.add('activo');
 });
+
+btnCategorias.addEventListener('mouseleave', () =>{
+    grid.classList.remove('activo');
+});
---
+++ views/public/AgregarProducto.php
@@ -0,0 +1,14 @@
+<?php
+//Se incluye la plantilla del encabezado para la página web
+include("../../app/helpers/header.php");
+?>
+
+
+ <div class="col-12 col-lg-12 col-md-12">
+            <h1>¿Desea agregar un nuevo producto?</h1>
+        </div>
+
+<?php
+//Se incluye la plantilla del encabezado para la página web
+include("../../app/helpers/footer.php");
+?>
