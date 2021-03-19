4735d95c6f959966a7f525cc241b28af4c4a45e4
SeishinF <fernatoolsgamer@gmail.com>
2021-03-18 18:59:12 -0600

    Creacion nuevos documentos y cambios generales

--- app/helpers/footer.php
+++ app/helpers/footer.php
@@ -1,14 +1,18 @@
-</main>
 
-    <aside class="text-center text-lg-start" id="ptFooter">
-        
-        <div class="container p-4  text-center">
-          
-          <div class="row">
+    </main>
+
+
+
+<aside class="text-center text-lg-start" id="ptFooter">
+
+    <div class="container p-4  text-center">
+
+        <div class="row">
             <!-- Div para Informacion -->
             <div class="col-12 col-lg-6 col-md-12">
                 <h1>¿Quienes Somos?</h1>
-                <p>Green Zone, es una tienda en linea dedicada a la venta de productos ecoamigables, con el objetivo de promover el uso de estos y combatir pasivamente la contaminacion de ciertos produtos</p>
+                <p>Green Zone, es una tienda en linea dedicada a la venta de productos ecoamigables, con el objetivo
+                    de promover el uso de estos y combatir pasivamente la contaminacion de ciertos produtos</p>
             </div>
             <!-- Div para Contacto -->
             <div class="col-12 col-lg-3 col-md-6 ">
@@ -31,7 +35,7 @@
             <div class="col-12 col-lg-3 col-md-6 mb-4 mb-md-0 ">
                 <div class="row">
                     <div class="col-12">
-                        <h5 class="text-uppercase">Nuestras Redes Sociales</h5>
+                        <h5 class="text-uppercase">Nuestras Redes</h5>
                     </div>
                     <div class="col-6">
                         <i class="fab fa-instagram fa-4x"></i>
@@ -42,24 +46,27 @@
                 </div>
             </div>
 
-          </div>
-          
         </div>
-        
-      <footer>
+
+    </div>
+
+    <footer>
         <!-- Derechos Reservados -->
         <div class="text-center p-3" id="ptDerechos">
             @2021 Derechos Reservados:
-              <a class="text-white" href="#">Green Zone </a>
-            </div>
-      </footer>
-        
-    </aside>
+            <a class="text-white" href="#">Green Zone </a>
+        </div>
+    </footer>
 
-    <!-- Agregamos SCRIPTS -->
-    <script src="https://kit.fontawesome.com/592eb2e9e3.js" crossorigin="anonymous"></script>
-    <script src="../../resources/js/bootstrap.min.js"></script>
-    <script src="../../resources/js/MenuInferior/mnInferior.js"></script>
+</aside>
+
+<!-- Agregamos SCRIPTS -->
+<script src="https://kit.fontawesome.com/592eb2e9e3.js" crossorigin="anonymous"></script>
+<script src="../../resources/js/bootstrap.min.js"></script>
+<script src="../../resources/js/MenuInferior/mnInferior.js"></script>
+<script src="../../resources/js/IndexSlider/index.js"></script>
+
+<script src="../../resources/js/IndexSlider/lightslider.js"></script>
 </body>
 
 </html>
