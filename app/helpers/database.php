6b8072c359d34beb3ed595b3f44236b4a25e05f5
FabioEstrada23 <65871577+FabioEstrada23@users.noreply.github.com>
2021-05-11 08:26:26 -0600

    Update database.php

--- app/helpers/database.php
+++ app/helpers/database.php
@@ -1,87 +1,47 @@
 <?php
-/* Clase para realizar las operaciones en la base de datos de GreenZone. */
-<<<<<<< Updated upstream
-class Database{
-
-        private static $connection;
-=======
-<<<<<<< HEAD
-    class Database{
-        private static $connection = null;
-=======
-class Database{
-
-        private static $connection;
->>>>>>> 9666605f975d90752523631bda45f3e62f2fdb77
->>>>>>> Stashed changes
-        private static $statement = null;
-        private static $error = null;
-
-        public static function connect()
-        {
-            // Credenciales para establecer la conexión con la base de datos.
-            $server = 'localhost';
-            $database = 'Green_Zone';
-            $username = 'postgres';
-            $password = '02032003';
-
-            if(!isset(self::$connection)){/* Validar que hay conexión*/ 
-                try{
-                    self::$connection = new PDO('pgsql:host='.$server.';dbname='.$database.';port=5432', $username, $password);
-                    /* Conexión con postgres*/
-                    self::$connection->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
-                    /* Parámetros*/
-                    self::$connection->exec("SET NAMES 'UTF8'");
-                    /* Respetar caracteres como ñ y acentos de BD */
-                }
-                catch(PDOException $e){
-                    print "ERROR: " .$e->getMessage()."<br/>";
-                    /* Imprimir mensaje de error */
-                }
-            }
-        }
-<<<<<<< Updated upstream
-    // Database::connect();
-
-    public static function executeRow($query, $values)
-=======
-<<<<<<< HEAD
+/*
+*   Clase para realizar las operaciones en la base de datos.
+*/
+class Database
+{
+    // Propiedades de la clase para manejar las acciones respectivas.
+    private static $connection = null;
+    private static $statement = null;
+    private static $error = null;
 
     /*
-    *   Método para obtener todos los registros de una sentencia SQL tipo SELECT.
+    *   Método para establecer la conexión con el servidor de base de datos.
+    */
+    private static function connect()
+    {
+        // Credenciales para establecer la conexión con la base de datos.
+        $server = 'localhost';
+        $database = 'Green_Zone';
+        $username = 'postgres';
+        $password = '02032003';
+
+        self::$connection = new PDO('pgsql:host='.$server.';dbname='.$database.';port=5432', $username, $password);
+            
+        
+        // Se crea la conexión mediante la extensión PDO y el controlador para PostgreSQL.
+        }
+
+    /*
+    *   Método para ejecutar las siguientes sentencias SQL: insert, update y delete.
     *
     *   Parámetros: $query (sentencia SQL) y $values (arreglo de valores para la sentencia SQL).
-    *
-    *   Retorno: arreglo asociativo de los registros si la sentencia SQL se ejecuta satisfactoriamente o false en caso contrario.
+    *   
+    *   Retorno: booleano (true si la sentencia se ejecuta satisfactoriamente o false en caso contrario).
     */
-    public static function getRows($query, $values)
-=======
-    // Database::connect();
-
     public static function executeRow($query, $values)
->>>>>>> 9666605f975d90752523631bda45f3e62f2fdb77
->>>>>>> Stashed changes
     {
         try {
             self::connect();
             self::$statement = self::$connection->prepare($query);
-<<<<<<< Updated upstream
-=======
-<<<<<<< HEAD
-            self::$statement->execute($values);
-            // Se anula la conexión con el servidor de base de datos.
-            self::$connection = null;
-            return self::$statement->fetchAll(PDO::FETCH_ASSOC);
-=======
->>>>>>> Stashed changes
             $state = self::$statement->execute($values);
             // Se anula la conexión con el servidor de base de datos.
             self::$connection = null;
             return $state;
-<<<<<<< Updated upstream
-=======
->>>>>>> 9666605f975d90752523631bda45f3e62f2fdb77
->>>>>>> Stashed changes
         } catch (PDOException $error) {
             // Se obtiene el código y el mensaje de la excepción para establecer un error personalizado.
             self::setException($error->getCode(), $error->getMessage());
@@ -89,18 +49,13 @@ class Database{
         }
     }
 
-<<<<<<< Updated upstream
-=======
-<<<<<<< HEAD
     /*
-    *   Método para obtener un registro de una sentencia SQL tipo SELECT.
+    *   Método para obtener el valor de la llave primaria del último registro insertado.
     *
     *   Parámetros: $query (sentencia SQL) y $values (arreglo de valores para la sentencia SQL).
     *   
-    *   Retorno: arreglo asociativo del registro si la sentencia SQL se ejecuta satisfactoriamente o false en caso contrario.
+    *   Retorno: numérico entero (último valor de la llave primaria si la sentencia se ejecuta satisfactoriamente o 0 en caso contrario).
     */
-=======
->>>>>>> Stashed changes
     public static function getLastRow($query, $values)
     {
         try {
@@ -121,10 +76,13 @@ class Database{
         }
     }
 
-<<<<<<< Updated upstream
-=======
->>>>>>> 9666605f975d90752523631bda45f3e62f2fdb77
->>>>>>> Stashed changes
+    /*
+    *   Método para obtener un registro de una sentencia SQL tipo SELECT.
+    *
+    *   Parámetros: $query (sentencia SQL) y $values (arreglo de valores para la sentencia SQL).
+    *   
+    *   Retorno: arreglo asociativo del registro si la sentencia SQL se ejecuta satisfactoriamente o false en caso contrario.
+    */
     public static function getRow($query, $values)
     {
         try {
@@ -139,9 +97,29 @@ class Database{
             self::setException($error->getCode(), $error->getMessage());
             return false;
         }
-<<<<<<< Updated upstream
-=======
-<<<<<<< HEAD
+    }
+
+    /*
+    *   Método para obtener todos los registros de una sentencia SQL tipo SELECT.
+    *
+    *   Parámetros: $query (sentencia SQL) y $values (arreglo de valores para la sentencia SQL).
+    *
+    *   Retorno: arreglo asociativo de los registros si la sentencia SQL se ejecuta satisfactoriamente o false en caso contrario.
+    */
+    public static function getRows($query, $values)
+    {
+        try {
+            self::connect();
+            self::$statement = self::$connection->prepare($query);
+            self::$statement->execute($values);
+            // Se anula la conexión con el servidor de base de datos.
+            self::$connection = null;
+            return self::$statement->fetchAll(PDO::FETCH_ASSOC);
+        } catch (PDOException $error) {
+            // Se obtiene el código y el mensaje de la excepción para establecer un error personalizado.
+            self::setException($error->getCode(), $error->getMessage());
+            return false;
+        }
     }
 
     /*
@@ -187,64 +165,4 @@ class Database{
     {
         return self::$error;
     }
-
-    }
-    
-    
-    
-?>
-=======
->>>>>>> Stashed changes
-    }
-
-    public static function getRows($query, $values)
-    {
-        try {
-            self::connect();
-            self::$statement = self::$connection->prepare($query);
-            self::$statement->execute($values);
-            // Se anula la conexión con el servidor de base de datos.
-            self::$connection = null;
-            return self::$statement->fetchAll(PDO::FETCH_ASSOC);
-        } catch (PDOException $error) {
-            // Se obtiene el código y el mensaje de la excepción para establecer un error personalizado.
-            self::setException($error->getCode(), $error->getMessage());
-            return false;
-        }
-    }
-
-    private static function setException($code, $message)
-    {
-        // Se compara el código del error para establecer un error personalizado.
-        switch ($code) {
-            case '7':
-                self::$error = 'Existe un problema al conectar con el servidor';
-                break;
-            case '42703':
-                self::$error = 'Nombre de campo desconocido';
-                break;
-            case '23505':
-                self::$error = 'Dato duplicado, no se puede guardar';
-                break;
-            case '42P01':
-                self::$error = 'Nombre de tabla desconocido';
-                break;
-            case '23503':
-                self::$error = 'Registro ocupado, no se puede eliminar';
-                break;
-            default:
-                //self::$error = 'Ocurrió un problema en la base de datos';
-                self::$error = $message;
-        }
-    }
-
-    public static function getException()
-    {
-        return self::$error;
-    }
 }
-
-<<<<<<< Updated upstream
-=======
->>>>>>> 9666605f975d90752523631bda45f3e62f2fdb77
->>>>>>> Stashed changes
