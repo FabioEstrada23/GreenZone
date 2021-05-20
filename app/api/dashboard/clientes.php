43c8fb1899266ef83ed58581e128040f1ac30ea2
SeishinF <fernatoolsgamer@gmail.com>
2021-05-20 09:54:57 -0600

    XD

--- app/api/dashboard/clientes.php
+++ app/api/dashboard/clientes.php
@@ -89,23 +89,23 @@ if (isset($_GET['action'])) {
                     }
                 break;
 
-            case 'update':
-                   $_POST = $cliente->validateForm($_POST);
-                   if ($cliente->setId($_POST['id_cliente_user'])) {
-                    if ($data = $cliente->readOne()) {
-                        if($cliente->setEstadoCliente($_POST['estado_cliente'])){
-                                    if ($cliente->updateRow()) {
-                                            $result['status'] = 1;
-                                            $result['message'] = 'Cliente actualizado correctamente';
-                                    } else {
-                                            $result['exception'] = Database::getException();
-                                    }
-                        }else{
-                            $result['message'] = 'Direccion Incorrecta';
-                        }
-                    }
-                   }
-                break;
+            // case 'update':
+            //        $_POST = $cliente->validateForm($_POST);
+            //        if ($cliente->setIdClienteUser($_POST['id_cliente_user'])) {
+            //         if ($data = $cliente->readOne()) {
+            //             if($cliente->setIdEstadoCli($_POST['estado_cliente'])){
+            //                         if ($cliente->updateRow()) {
+            //                                 $result['status'] = 1;
+            //                                 $result['message'] = 'Cliente actualizado correctamente';
+            //                         } else {
+            //                                 $result['exception'] = Database::getException();
+            //                         }
+            //             }else{
+            //                 $result['message'] = 'Direccion Incorrecta';
+            //             }
+            //         }
+            //        }
+            //     break;
 
                 default:
                     $result['exception'] = 'Acción no disponible dentro de la sesión';
